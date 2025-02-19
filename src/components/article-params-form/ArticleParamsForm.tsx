8a0e2a72ed140bb710012bea6a2c8320df6c8ced
Sergei Suprun <sergeysuprun@list.ru>
2025-02-19 21:40:03 +0300

    done

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,27 +1,26 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Select } from 'src/ui/select';
-import { ArticleStateType, OptionType } from 'src/constants/articleProps';
-import { defaultArticleState } from 'src/constants/articleProps';
+import {
+	ArticleStateType,
+	OptionType,
+	defaultArticleState,
+	fontFamilyOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
 import styles from './ArticleParamsForm.module.scss';
-import { fontFamilyOptions } from 'src/constants/articleProps';
-import { fontColors, backgroundColors, contentWidthArr } from 'src/constants/articleProps';
 import { Separator } from 'src/ui/separator';
 import { RadioGroup } from 'src/ui/radio-group';
-import { fontSizeOptions } from 'src/constants/articleProps';
-import { useOutsideClickClose } from 'src/ui/select/hooks/useOutsideClickClose';
 import { Text } from 'src/ui/text';
-import { useEffect } from 'react';
 
-
-export const ArticleParamsForm:  React.FC<{
+export const ArticleParamsForm: React.FC<{
 	setDesignState: (state: ArticleStateType) => void;
 }> = ({ setDesignState }) => {
 	const [isOpen, setIsOpen] = useState(false);
-
-
-	const [overlay, setOverlay] = useState(0);
 
 	const [backgroundColor, setBackgroundColor] = useState(
 		defaultArticleState.backgroundColor
@@ -29,14 +28,9 @@ export const ArticleParamsForm:  React.FC<{
 	const [contentWidth, setContentWidth] = useState(
 		defaultArticleState.contentWidth
 	);
-	const [fontColor, setFontColor] = useState(
-		defaultArticleState.fontColor
-	)
+	const [fontColor, setFontColor] = useState(defaultArticleState.fontColor);
 
-	const [fontSize, setFontSize] = useState(
-		defaultArticleState.fontSizeOption
-	);
-
+	const [fontSize, setFontSize] = useState(defaultArticleState.fontSizeOption);
 
 	const [font, setFont] = useState<OptionType>(
 		defaultArticleState.fontFamilyOption
@@ -52,20 +46,19 @@ export const ArticleParamsForm:  React.FC<{
 			fontColor,
 			backgroundColor,
 			contentWidth,
-		})
+		});
 	};
 
 	const handleReset = () => {
 		setDesignState(defaultArticleState);
-		setFont(defaultArticleState.fontFamilyOption)
-		setFontSize(defaultArticleState.fontSizeOption)
-		setFontColor(defaultArticleState.fontColor)
-		setBackgroundColor(defaultArticleState.backgroundColor)
-		setContentWidth(defaultArticleState.contentWidth)
-	}
+		setFont(defaultArticleState.fontFamilyOption);
+		setFontSize(defaultArticleState.fontSizeOption);
+		setFontColor(defaultArticleState.fontColor);
+		setBackgroundColor(defaultArticleState.backgroundColor);
+		setContentWidth(defaultArticleState.contentWidth);
+	};
 
-
-useEffect(() => {
+	useEffect(() => {
 		if (!isOpen) return;
 
 		const handleClickOutside = (event: MouseEvent) => {
@@ -83,18 +76,17 @@ useEffect(() => {
 		};
 	}, [isOpen]);
 
-
 	return (
 		<>
 			<ArrowButton isOpen={isOpen} onClick={() => setIsOpen(!isOpen)} />
 			<aside
 				ref={sideBar}
 				className={styles.container}
-				style={{ transform: isOpen ? 'translateX(0)' : 'translateX(-100%)'}}>
-				<form className={styles.form}
-				onSubmit={handleSubmit}
-				onReset={handleReset}
-				>
+				style={{ transform: isOpen ? 'translateX(0)' : 'translateX(-100%)' }}>
+				<form
+					className={styles.form}
+					onSubmit={handleSubmit}
+					onReset={handleReset}>
 					<Text as='h2' size={31} weight={800} uppercase>
 						Задайте параметры
 					</Text>
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,24 @@
 import { createRoot } from 'react-dom/client';
 import React, { StrictMode, CSSProperties, useState } from 'react';
 
-import ReactDOM from 'react-dom/client';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState, ArticleStateType } from './constants/articleProps';
+import {
+	defaultArticleState,
+	ArticleStateType,
+} from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
-
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 export const App = () => {
-	const [designState, setDesignState] = useState<ArticleStateType>(defaultArticleState);
+	const [designState, setDesignState] =
+		useState<ArticleStateType>(defaultArticleState);
 
 	return (
 		<main
@@ -41,4 +43,3 @@ root.render(
 		<App />
 	</StrictMode>
 );
-
